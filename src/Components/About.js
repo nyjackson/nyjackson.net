9410929b9fb1eec0ed1acbb042fccb0e214dd867
njackson <nyalajackson@yahoo.com>
2021-08-22 20:59:09 -0400

    nyjackson.net 8.22.21
    Updates made to nyjackson.net react edition

---
+++ src/Components/About.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Fade from "react-reveal";
+
+class About extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const profilepic = "images/" + this.props.data.image;
+    const bio = this.props.data.bio;
+    const email = this.props.data.email;
+    const resumeDownload = this.props.data.resumedownload;
+
+    return (
+      <section id="about">
+        <Fade duration={1000}>
+          <div className="row">
+         <h1>About Me</h1>
+              <p>{bio}</p>
+                    <a href={resumeDownload} className="button">
+                      <i className="fa fa-download"></i>Download Resume
+                    </a>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default About;
